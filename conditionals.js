64e45b98860e3ce11dca6088775ef1c189562b0a
parthar409 <parthar40888@gmail.com>
2023-04-17 23:05:31 +0600

    conditionals operator use

---
+++ conditionals.js
@@ -0,0 +1,21 @@
+// var biscutPrice =8;
+// if(biscutPrice >10){
+//     console.log("i will eat tha biscut");
+// }
+// else{
+//     console.log("Kali cha is better");
+// }
+
+
+// var jobPaiso=true;
+// var savingAmount = 5000;
+// if(jobPaiso==true && savingAmount >200000){
+// console.log("aso biya kori");
+// }
+// else if(jobPaiso==true)
+// {
+//     console.log("tomar jonno vabte hobe");
+// }
+// else{
+// console.log("tor kopale biya nai");
+// }
