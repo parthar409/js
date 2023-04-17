3067c3ae9f9b8cf477e989be5b53ce9ac540c5c5
parthar409 <parthar40888@gmail.com>
2023-04-17 22:32:18 +0600

    basic js

---
+++ hello.js
@@ -0,0 +1,50 @@
+var bananaPrice = 12;
+console.log(bananaPrice);
+var seenAfter =21;
+console.log(typeof seenAfter);
+
+var name = "Partha sarathi roy";
+console.log(typeof name);
+
+var isHot = true;
+console.log(typeof isHot);
+
+var  name = "HERO ALOM"
+console.log(name);
+var promise ="i promisee  i will work hard to become  a programmer";
+// console.log(promise.toLowerCase());
+console.log(promise.toUpperCase());
+console.log(promise.indexOf('work'));
+console.log(promise.split(''));
+
+var number1 =24;
+var number2 ='23.7';
+// parseFloat......parseInt...+
+// number2 =parseFloat(number2);
+number2 = +number2;
+// console.log(number1+number2);
+var total =number1+number2;
+// total = total.toFixed(1);
+console.log(total);
+
+
+var price1=30;
+var price2=50;
+// var prices=price1+price2;
+// var prices=price1-price2;
+// var prices=price1*price2;
+// var prices=price1/price2;
+var prices=price2%price1;
+console.log(prices);
+
+
+
+
+// math.abs  - namber + result
+// math.round .4.9=0,.5=1 result
+
+
+// lottari
+var number4= Math.random()*100;
+var lottari= Math.round(number4);
+console.log(lottari);
