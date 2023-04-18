1013846eeefce131c10c0772bbe8a0d61da53156
parthar409 <parthar40888@gmail.com>
2023-04-18 18:37:26 +0600

    js --apply

---
+++ apply-js/constLet.js
@@ -0,0 +1,9 @@
+var name = 'javed'
+if(name.length >4){
+ name = 'jova';
+}
+console.l og(name);
+
+
+
+// let constant use this vatiabole declard
---
+++ apply-js/leapYear.js
@@ -0,0 +1,12 @@
+const year =3996;
+let remainder = year % 4;
+if(remainder == 0){
+    console.log("Your year is a leep year");
+}
+else{
+    console.log('your year is  not a leep year');
+}
+
+
+
+// this js funtion chack
