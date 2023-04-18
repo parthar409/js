2946165f07b9f62d5caac70f91cd6071b2364660
parthar409 <parthar40888@gmail.com>
2023-04-18 13:09:01 +0600

    array basic complet

--- array.js
+++ array.js
@@ -1,6 +1,17 @@
-var firendsAge =[15,17,14,18,19,10];
+// var firendsAge =[15,17,14,18,19,10];
 // console.log(firendsAge[4]);
-var sonaliAge = firendsAge[2]
-firendsAge[1]=40;
+// var sonaliAge = firendsAge[2]
+// firendsAge[1]=40;
 // console.log(sonaliAge);
-console.log(firendsAge);
+// console.log(firendsAge.length); 
+
+
+
+var teaLine =['partha,dalim,joydip'];
+teaLine.push('jalam');
+teaLine.pop();
+teaLine.unshift('hemal');
+console.log(teaLine);
+
+
+ 
