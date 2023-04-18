6bd9b1b2a7f9bbc020220835e8e76841a1129c99
parthar409 <parthar40888@gmail.com>
2023-04-18 14:42:04 +0600

    js apply

---
+++ apply-js/inchToFeet.js
@@ -0,0 +1,13 @@
+function inchToFeet(inch){
+ var feet = inch/12;
+ return feet;
+}
+var nanafeet =inchToFeet(233);
+nanafeet = nanafeet.toFixed(2);
+console.log(nanafeet);
+
+var nanifeet =inchToFeet(3445);
+nanifeet =nanifeet.toFixed(2);
+console.log(nanifeet);
+
+
